5c289545aa97504188dee1a65ceda5460f296b74
ykokw <0k4w4.yuk1@gmail.com>
2020-02-02 20:42:25 +0900

    Added decks Fixed index to list all decks

---
+++ decks/customTheme.js
@@ -0,0 +1,33 @@
+const white = '#fff'
+const gray = '#333'
+const red = '#f00'
+
+export default {
+  fonts: {
+    body: '"Helvetica Neue", Helvetica, Arial, sans-serif',
+  },
+  colors: {
+    text: gray,
+    background: white,
+    primary: red,
+  },
+  styles: {
+    root: {
+      textAlign: 'left',
+      fontSize: [null, null, null, '1.2em', '1.6em', '2.4em'],
+    },
+    Slide: {
+      display: 'block',
+      padding: '2em',
+      textAlign: 'left',
+    },
+    ul: {
+      fontSize: '1.2rem',
+      paddingLeft: '21px',
+    },
+    h2: {
+      paddingLeft: '16px',
+      borderLeft: '4px solid blue'
+    },
+  },
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,36 @@
 import React from "react"
-import { Link } from "gatsby"
+import { useStaticQuery, graphql, Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const IndexPage = () => {
+  const data = useStaticQuery(graphql`
+    query AllDecksQuery {
+      allDeck {
+        edges {
+          node {
+            slug
+            title
+          }
+        }
+      }
+    }
+  `)
+  const {
+    allDeck: { edges },
+  } = data
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1>mdx-decks-example</h1>
+      <ul>
+      {edges.map(({ node }) => (
+        <li><a href={`${node.slug}`} target='_blank'>{node.title}</a></li>
+      ))}
+      </ul>
+    </Layout>
+  )
+}
 
 export default IndexPage
